0af664c7fc261e995ded459d41964cefe0a34501
Lee seungri <leevictory7272@gmail.com>
2023-08-14 18:39:24 +0900

    feat : routing path와 파일 정렬 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import AppRouter from "./AppRouter";
+import { GlobalStyles, theme } from "./styles";
+
+import AppRouter from "./Routers/routing.tsx";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return(
-     <AppRouter />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppRouter />
+      </ThemeProvider>
   );
 }
 
