e95d4112ef95e34a2f074959b4dc07832d95cf92
Doeunnkimm <doeunnkimm@gmail.com>
2023-08-14 12:45:25 +0900

    feat: Box 공용 컴포넌트 추가

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './Box.styles'
+
+import type { FC, HTMLAttributes, PropsWithChildren } from 'react'
+
+import type { StyleProps } from './Box.types'
+
+interface Props extends HTMLAttributes<HTMLDivElement>, StyleProps {}
+
+const Box: FC<Props> = ({ children, ...props }: PropsWithChildren<Props>) => {
+  return <S.Box {...props}>{children}</S.Box>
+}
+
+export default Box
