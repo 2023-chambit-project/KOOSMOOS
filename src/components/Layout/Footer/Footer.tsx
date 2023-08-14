e3ef503c20ae2677137195739a4278446df09b7b
Doeunnkimm <doeunnkimm@gmail.com>
2023-08-14 18:44:59 +0900

    feat: footer 컴포넌트 작업

---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import * as S from './Footer.styles'
+
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <S.MenuTab>
+        <li>홈</li>
+        <li>뉴스</li>
+        <li>테크위치</li>
+        <li>게임</li>
+        <li>갤러리</li>
+      </S.MenuTab>
+      <S.Line>
+        <S.IconMap>
+          <FontAwesomeIcon icon={faEnvelope} size="xl" style={{ color: '#ffffff' }} />
+        </S.IconMap>
+      </S.Line>
+      <S.BottomTab>
+        <S.Logo src="assets/logo/logo_512x105.svg" />
+        <div>
+          <p>koosmoos@gmail.com</p>
+          <p>ⓒ 2023 Kwangwoon univ, Team LOSS All Rights Reserved.</p>
+        </div>
+      </S.BottomTab>
+    </S.Footer>
+  )
+}
+
+export default Footer
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer/Footer'
 import Header from './Header/Header'
 
 export default function Layout() {
@@ -6,6 +7,7 @@ export default function Layout() {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
