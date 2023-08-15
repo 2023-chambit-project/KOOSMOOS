a17f4f14afa95136a1ee450c2aaeabdca7e4929a
yunshin <2younsin@gmail.com>
2023-08-15 14:09:39 +0900

    refactor: color, size 속성 기본값 제공 & 속성값 부여 방법 변경

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,15 +1,15 @@
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FC } from 'react'
 
 interface Props {
   color?: string
-  size?: string
+  size?: SizeProp
   icon: IconProp
   strokeWidth?: number
   strokeColor?: string
 }
 
-export const Icon: FC<Props> = ({ icon, color, size, ...rest }) => (
-  <FontAwesomeIcon icon={icon} style={{ color: color, fontSize: size }} {...rest} />
+export const Icon: FC<Props> = ({ icon, color = 'white', size = '1x', ...rest }) => (
+  <FontAwesomeIcon icon={icon} color={color} size={size} {...rest} />
 )
