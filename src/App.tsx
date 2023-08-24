0793381eaeadddd47b073a7d395647b71a225bb3
Doeunnkimm <doeunnkimm@gmail.com>
2023-08-25 00:23:20 +0900

    chore: @tanstack/react-query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { GlobalStyles, theme } from './styles'
 
 import { routers } from '@/routes/routing'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+      },
+    },
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <RouterProvider router={routers} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <RouterProvider router={routers} />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
