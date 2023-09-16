8b151e8665f2f09ccbc4e84e3fc1848f0d3296eb
Doeunnkimm <doeunnkimm@gmail.com>
2023-09-16 15:11:31 +0900

    feat: WikiPost 컴포넌트 추가 및 페이지 구성

---
+++ src/pages/Tech-wiki/WikiPost/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './WikiPost.styles'
+import { Post } from './components'
+
+const WikiPostPage = () => {
+  return (
+    <S.Wrapper>
+      <Post />
+    </S.Wrapper>
+  )
+}
+
+export default WikiPostPage
