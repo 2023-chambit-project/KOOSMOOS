b2cbe435a2e47eac6b3597844a9e61b6b6df1e25
yunshin <2younsin@gmail.com>
2023-09-27 22:41:53 +0900

    chore: 호버 상태 시, 그림자 효과 제거

--- src/components/ButtonV2/ButtonV2.styles.ts
+++ src/components/ButtonV2/ButtonV2.styles.ts
@@ -37,10 +37,21 @@ export const skinCSS = {
   default: css`
     color: ${({ theme }) => theme.COLOR.common.white};
     border-color: ${({ theme }) => theme.COLOR.common.white};
+
+    &:hover {
+      color: ${({ theme }) => theme.COLOR.common.black};
+      border-color: ${({ theme }) => theme.COLOR.common.black};
+      background-color: ${({ theme }) => theme.COLOR.common.white};
+    }
   `,
   dark: css`
     color: ${({ theme }) => theme.COLOR.common.black};
     border-color: ${({ theme }) => theme.COLOR.common.black};
+    &:hover {
+      color: ${({ theme }) => theme.COLOR.common.white};
+      border-color: ${({ theme }) => theme.COLOR.common.white};
+      background-color: ${({ theme }) => theme.COLOR.common.black};
+    }
   `,
 }
 
@@ -56,12 +67,10 @@ export const ButtonV2 = styled.button<StyleProps>`
   ${({ skin = 'default' }) => skinCSS[skin]}
   background-color: ${({ bgColor, theme }) => (bgColor ? bgColor : theme.COLOR.common.gray[900])};
 
-  &:hover {
-    box-shadow:
-      inset 0 2px 20px rgba(0, 0, 0, 0.2),
-      0 2px 20px rgba(0, 0, 0, 0.2);
-  }
-  transition: box-shadow 0.3s ease;
+  transition:
+    color 0.1s ease,
+    background-color 0.2s ease,
+    border-color 0.3s ease;
 
   white-space: nowrap;
   overflow: hidden;
