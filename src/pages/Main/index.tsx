9443e3854a7e7fea7aa2b2ced1c9493de583631a
Doeunnkimm <doeunnkimm@gmail.com>
2023-11-12 15:09:49 +0900

    feat: 렌딩페이지 퍼블리싱

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,23 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import * as S from './Main.styles'
+import { LandingSection1, LandingSection2, LandingSection3 } from './section'
+
 const MainPage = () => {
-  return <h1>MainPage</h1>
+  return (
+    <S.MainContainer>
+      <Parallax pages={3}>
+        <ParallaxLayer offset={0}>
+          <LandingSection1 />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <LandingSection2 />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2}>
+          <LandingSection3 />
+        </ParallaxLayer>
+      </Parallax>
+    </S.MainContainer>
+  )
 }
 
 export default MainPage
