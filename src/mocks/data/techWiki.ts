b0ac0ff5366391c70bc44e3c436191f088636b4c
김도은 <doeunnkimm@gmail.com>
2023-12-06 17:20:23 +0900

    Tech wiki 글 업로드 했습니다.

--- src/mocks/data/techWiki.ts
+++ src/mocks/data/techWiki.ts
@@ -4,31 +4,14 @@ export const techWikiPostsSummary: TechWikiPostSummaryProps[] = [
   {
     id: '1',
     thumbnail: `/assets/tech-wiki/list-thumbnail/thumbnail1.png`,
-    title: '1번 포스트입니다.',
-    description:
-      '천체를 관측해요 천체를 관측해요 천체를 관측해요 천체를 관측해요. 천체를 관측해요 천체를 관측해요 천체를 관측해요',
-    date: '2023-09-15',
+    title: '민간인 우주여행에 대해서',
+    description: '우주 여행을 떠나는 꿈을 꾸어본 적이 있나요?항상 보고있었지만, 닿지 못했던 그곳',
+    date: '2023-12-06',
   },
   {
     id: '2',
     thumbnail: `/assets/tech-wiki/list-thumbnail/thumbnail2.png`,
-    title: '2번 포스트입니다.',
-    description:
-      '천체를 관측해요 천체를 관측해요 천체를 관측해요 천체를 관측해요. 천체를 관측해요 천체를 관측해요 천체를 관측해요',
-    date: '2023-09-14',
-  },
-  {
-    id: '3',
-    thumbnail: `/assets/tech-wiki/list-thumbnail/thumbnail1.png`,
-    title: '3번 포스트입니다.',
-    description:
-      '천체를 관측해요 천체를 관측해요 천체를 관측해요 천체를 관측해요. 천체를 관측해요 천체를 관측해요 천체를 관측해요',
-    date: '2023-09-15',
-  },
-  {
-    id: '4',
-    thumbnail: `/assets/tech-wiki/list-thumbnail/thumbnail2.png`,
-    title: '4번 포스트입니다.',
+    title: '대한민국의 누리호',
     description:
       '천체를 관측해요 천체를 관측해요 천체를 관측해요 천체를 관측해요. 천체를 관측해요 천체를 관측해요 천체를 관측해요',
     date: '2023-09-14',
@@ -38,89 +21,214 @@ export const techWikiPostsSummary: TechWikiPostSummaryProps[] = [
 export const TechWikiPosts: TechWikiPostProps[] = [
   {
     id: '1',
-    title: '1번 포스트입니다.',
+    title: '민간인 우주여행에 대해서',
     thumbnail: `/assets/tech-wiki/post-thumbnail/thumbnail1.png`,
-    date: '2023-09-11',
+    date: '2023-12-06',
     writer: {
       nickname: 'TransparentDeveloper',
       profile_img: 'https://avatars.githubusercontent.com/u/50646145?v=4',
     },
-    content: `안녕하세요. 팀 LOSS의 **TransparentDeveloper**입니다. 
+    content: `# "민간인 우주여행에 대해서"
 
-<br />
+<div align="center">
     
-## 우주 관측하시는 걸 좋아하시나요?
+![peace_space](https://github.com/TransparentDeveloper/KOOSMOOS-TechWiki/assets/50646145/22ae5f6a-4ff1-47a0-99d6-6c6b4d858982)
+    
+**👨‍🚀 모험가 여러분! 안녕하세요.👋🏼**
+    
+**지구를 떠나 별 너머, 우주 여행을 떠나는 꿈을 꾸어본 적이 있나요?항상 보고있었지만, 닿지 못했던 그곳!!**
+    
+**📢 KOSSMOSS 에서 멀지 않지만 가깝지도 않았던 우주로의 여행이 곧 시작됨을 알려드립니다.**
+    
+</div>
+    
+<br/>
+<br/>
+<br/>
+    
+## 우주 관광은 더 이상 공상 과학 소설에만 국한된 개념이 아닙니다.
+    
+⭐️ 최근 몇 년 동안 SpaceX (스페이스엑스)와 Virgin Galactic (버진 갤럭틱), 그리고 Blue Origin (블루 오리진) 과 같은 우주 항공 기업들이 우주 여행의 꿈을 실질적인 현실로 바꾸었습니다.
+    
+<div align="center">
+    
+![blue-origin 2](https://github.com/TransparentDeveloper/KOOSMOOS-TechWiki/assets/50646145/16663c7e-365e-46ce-89bc-461231d57fb9)
+    
+![virgin 1](https://github.com/TransparentDeveloper/KOOSMOOS-TechWiki/assets/50646145/1737be74-e395-4084-9b9b-8170c2a1a6c0)
+    
+![spaceX 1](https://github.com/TransparentDeveloper/KOOSMOOS-TechWiki/assets/50646145/ef8adf48-f99a-432c-9600-5974ce9f7d46)
+    
+</div>
+    
+<br/>
+<br/>
+<br/>
+    
+## 혁신적인 기술 발전과 획기적인 아이디어 로 우주 가는 길을 열리고 있습니다. 👐
+    
+2021년, Virgin Galactic의 VSS Unity가 최초로 민간인을 태운 채, 준궤도 우주 비행을 성공하면서 민간인 우주 관광의 역사의 시작되었습니다.
+이날 탑승했던 인원의 대부분은 훈련된 군인이나 우주비행사가 아니라 민간인이었습니다 🤩
+    
+우주는 더 이상 우주 비행사 만을 위한 것이 아니라 여러분과 저와 같이, 모험을 즐기는 사람들의 것임이 증명되고 있는 것만 같습니다. 🥹
+    
+<div align="center">
+    
+[![VSS Unity's First Spaceflight](http://img.youtube.com/vi/h8T9mVkGh3s/sddefault.jpg)](https://www.youtube.com/watch?v=h8T9mVkGh3s?t=0s)
+    
+_(👆 위 이미지를 클릭해보세요.)_
+    
+_유인 우주선 '유니티'가 모선 '이브’와 함께 활주로를 달린 뒤, 14km 상공에서 분리되어 고도 89km 까지 날아올랐습니다._
+    
+_☝️ 참고로 미항공우주국, NASA 는 해발고도 80 km 부터 우주라고 정의했답니다.!_
+    
+</div>
+    
+<br/>
+    
+<div align="center">
+    
+.
+    
+.
 
-<br />
-
-<img src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb2rbPt%2Fbtqy0FJgja1%2FgBn90IqzhQj3wT1axgAiiK%2Fimg.jpg" />
-
-__참 이쁘죠?__
-    `,
+.
+    
+.
+    
+.
+    
+</div>
+    
+<br/>
+    
+</div>
+    
+Amazon의 Jeff Bezos가 설립한 Blue Origin 은 우주 관광 서비스를 제공하는 또 다른 주요 기업입니다. 그들 역시 뉴 셰퍼드(New Shepard) 라는 자체 개발한 로켓을 발사하여, 11분간의 준궤도 여행을 통해 승객들에게 무중력 상태와 숨 막힐 듯한 지구의 전망을 선사했습니다. 🌏💫
+    
+<div align="center">
+    
+[![Replay - New Shepard First Human Flight](http://img.youtube.com/vi/tMHhXzpwupU/sddefault.jpg)](https://www.youtube.com/watch?v=tMHhXzpwupU?t=0s)
+    
+_(👆 위 이미지를 클릭해보세요.)_
+    
+</div>
+    
+<br/>
+    
+<div align="center">
+    
+.
+    
+.
+    
+.
+    
+.
+    
+.
+    
+</div>
+    
+<br/>
+    
+Elon Musk (일론 머스크) 의 SpaceX 는 지난 2022년, 세계 최초로 민간인을 태운 채 국제 우주정거장에 방문한 프로젝트를 성공했습니다. 이 당시, 발사된 Crew Dragon (크루 드래곤, SpaceX 가 개발한 유인 우주선) 에는 3명의 민간인 손님이 탑승하였는데요. 목적지인 우주정거장에 무려 8일을 머무리는 일정을 소화했습니다. 😱
+    
+<div align="center">
+    
+[![Crew-1 Mission | Launch](http://img.youtube.com/vi/bnChQbxLkkI/sddefault.jpg)](https://www.youtube.com/watch?v=bnChQbxLkkI?t=15139s)
+    
+_(👆 위 이미지를 클릭해보세요.)_
+    
+</div>
+    
+<br/>
+<br/>
+<br/>
+    
+### 우주 여행은 호기심과 모험심 넘치는 사람들에게 우주를 열어주는 산업입니다.
+    
+상상만 해도 시도할 수 있는 분야는 무한해 보이지 않나요??😎
+    
+스페이스 뷰 호텔, 행성 간 휴게소, 화성 머드 축제, 태양열 찜질방, 등등등...
+    
+언제가 우주 항공권을 예약해, 지구 밖으로 휴가를 떠나는 미래가 여러분의 일상이 되기를 바라며 🥹🙏
+    
+KOOSMOOS 에서 남은 여행, 즐거운 경험하시길 바랍니다! 🚀✨
+    
+<br/>
+<br/>
+<br/>
+    
+### 여기도 방문해 보세요!!
+    
+- [➡️ SpaceX 방문하기](https://www.spacex.com/)
+- [➡️ Virgin Galactic 방문하기](https://www.virgingalactic.com/)
+- [➡️ Blue Origin 방문하기](https://www.blueorigin.com/)`,
   },
   {
     id: '2',
-    title: '2번 포스트입니다.',
+    title: '대한민국의 누리호',
     thumbnail: `/assets/tech-wiki/post-thumbnail/thumbnail1.png`,
-    date: '2023-09-12',
-    writer: {
-      nickname: 'Doeunnkimm',
-      profile_img: 'https://avatars.githubusercontent.com/u/112946860?v=4',
-    },
-    content: `
-안녕하세요. 팀 LOSS의 Doeunnkimm입니다.
-
-# 우리 가까운 곳에 있는 학교에서 별 보기를 시도해보세요!.
-
-<img src="https://www.kw.ac.kr/KWData/webeditor/2020/2020_05_29_102245.jpg" />
-
-**하늘을 바라봅시다! 🌛**
-    `,
-  },
-  {
-    id: '3',
-    title: '3번 포스트입니다.',
-    thumbnail: `/assets/tech-wiki/post-thumbnail/thumbnail1.png`,
-    date: '2023-09-13',
+    date: '2023-12-06',
     writer: {
       nickname: 'smilevictory',
       profile_img: 'https://avatars.githubusercontent.com/u/113532580?v=4',
     },
     content: `
-안녕하세요. 팀 LOSS의 smilevictory입니다!
+# 대한민국의 누리호
+
+> 대한민국 최초의 저궤도 실용 위성 발사용 로켓
+
+<br />
+<br />
+    
+## 누리호란?
+
+
+    
+> KSLV-Ⅱ(Korea Space Launch Vehicle-Ⅱ, 한국형발사체-Ⅱ)라고도 불린다. 누리호는 한국항공우주연구원이 KSLV 계획에 따라 2022년 개발하여 운용 중인 로켓이다.
+
+<p align="center"><img width="329" alt="kslv" src="https://github.com/2023-chambit-project/KOOSMOOS-TechWiki/assets/113532580/ac2e5bbd-0724-46a0-b113-69e15f50d734"></p>
+
+> 누리호의 발사로 한국은 세계 11번째의 자력 우주로켓 발사국이 되었다.
+> 1톤 이상의 실용 위성을 궤도에 안착시킬 수 있는 7개국 반열에 올랐다. 누리호 개발사업은 나로호에 투입된 예산 5천억 원의 4배인 2조 원이 투입된 사업이며 누리호에 사용되는 기술들은 향후 개발할 KSLV-3의 기술적 기반이 될 예정이다.
+> 누리호는 총 3단의 액체로켓으로 구성되어 있다. 1단에는 추력 735 kN의 75톤급 엔진 4개를 클러스터링하여 총 300톤의 추력을, 2단에는 75톤급 엔진 하나를 사용한다. 3단에는 7톤급 엔진 하나를 사용한다. 모든 엔진은 KARI에서 개발하였다.
+
+<p align="center"><img width="445" alt="누리호 구조" src="https://github.com/2023-chambit-project/KOOSMOOS-TechWiki/assets/113532580/39c0b509-e0f0-423a-9945-1664c5ccb0af"></p>
+
+<br />
+<br />
+    
+### 누리호 1차 발사
+    
+> - 1차 발사일은 2021년 10월 21일 목요일 오후 5시에 진행되었다.
+> - 발사 결과는 궤도 진입 실패이다.
+> - 실패 원인
+>
+> 1. 비행가속도 증가로 인해 3단 산화제 탱크 안의 액체 산소 부력이 상승
+>
+> 2. 산화제 탱크 안 헬륨 탱크의 고정장치가 풀리며 배관을 변형시켜 헬륨이 누설
+>
+> 3. 산화제 탱크의 균열을 발생시켜 산화제가 누설되었다.
+>    목표 궤도 700km에는 도달했지만 초속 7.5km 속도는 도달하지 못했다.
 
 <br />
 
-### 유명한 별 보기 좋은 곳을 소개해 드립니다 :)
+### 누리호 2차 발사
+    
+> - 2차 발사일은 2022년 6월 21일에 진행되었다.
+> - 누리호 개발 사업의 마지막 단계이자 마지막 시험발사이다.
+> - 1차 발사와는 달리 1.3톤의 위성모사체와 함께 0.2톤의 큐브위성 4기를 포함한 성능검증위성을 탑재했다.
+> - 이때부터 실제 위성을 싣고 쏘게 되며, 6월 21일 성공적으로 발사하였음으로 3차 발사부터는 실용위성을 발사하게 된다.
 
 <br />
 
-<img src="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/e039d050-bab4-4beb-ac63-ad1356605326.jpeg" />
-
-**자세한 위치는 아래에서 소개해 드리겠습니다!**
-    `,
-  },
-  {
-    id: '4',
-    title: '4번 포스트입니다.',
-    thumbnail: `/assets/tech-wiki/post-thumbnail/thumbnail1.png`,
-    date: '2023-09-13',
-    writer: {
-      nickname: 'smilevictory',
-      profile_img: 'https://avatars.githubusercontent.com/u/113532580?v=4',
-    },
-    content: `
-안녕하세요. 팀 LOSS의 smilevictory입니다!
-
-<br />
-
-# 이건 4번 포스팅입니다 !!
-
-<br />
-
-<img src="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/e039d050-bab4-4beb-ac63-ad1356605326.jpeg" />
-
-**좋은 글을 소개해 드리고 있으니 다른 포스팅도 살펴보세요!**
-    `,
+### 누리호 3차 발사
+> - 3차 발사일은 2023년 5월 25일에 진행되었다.
+> - 발사체의 발사는 성공적이다.
+> - 큐브위성 7기와 차세대 소형위성를 탑재했다.
+> - 1단과 2단을 성공적으로 분리했다. 이후로도 누리호는 정상 비행을 이어갔고,  목표 고도에 도달했다.
+> - 목표 고도에 도달한 누리호는 먼저 차세대 소형위성 2호 분리에 성공했다. 이후 20초 간격으로 도요샛 4기 중 3호기를 제외한 3기를  떼어내는 데 성공했다.
+>   (위성 분리가 모두 종료되었다.)`,
   },
 ]
