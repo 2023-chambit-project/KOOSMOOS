1ba23ab6d2ae6840f4aa249cd292aea9c6ee4c91
smilevictory <leevictory7272@gmail.com>
2023-12-06 14:52:08 +0900

    refactor : (이미지) 탭 -> 레이어 수정

--- src/pages/Gallery/Gallery.styles.ts
+++ src/pages/Gallery/Gallery.styles.ts
@@ -47,3 +47,21 @@ export const TopButton = styled.button`
   font-size: 16px;
   margin-bottom: 20px;
 `
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ModalContent = styled.div`
+  padding: 10px;
+  background: white;
+  border-radius: 8px;
+`
--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,7 +1,6 @@
+/*
 import * as S from './Gallery.styles'
-
 import { useEffect, useState } from 'react'
-
 import MenuBar from './components/Category'
 import List from './components/PictureList'
 import { categoryMap } from './gallery.constants'
@@ -38,6 +37,67 @@ const GalleryPage = () => {
     <S.Wrapper>
       <MenuBar setSelectedCategory={setSelectedCategory} />
       <List pictures={pictures} onImageClick={handleImageClick} />
+    </S.Wrapper>
+  )
+}
+
+export default GalleryPage
+*/
+
+import * as S from './Gallery.styles'
+
+import { useEffect, useState } from 'react'
+
+import List from './components/PictureList'
+import MenuBar from './components/Category'
+import { categoryMap } from './gallery.constants'
+
+const GalleryPage = () => {
+  const [data, setData] = useState('')
+  const [pictures, setPictures] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState<'행성' | '우주' | '발사체' | '인공위성'>('행성')
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const [selectedImage, setSelectedImage] = useState<string>('')
+
+  useEffect(() => {
+    const apiUrl = `https://images-api.nasa.gov/search?q=${categoryMap[selectedCategory]}&media_type=image`
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const photos = data.collection.items.map((item: any) => ({
+          imageUrl: item.links[0].href,
+          title: item.data[0].title,
+          links: item.links,
+        }))
+
+        setPictures(photos.slice(0, 16))
+      })
+      .catch((error) => {
+        console.error('Failed to load picture data.', error)
+      })
+  }, [selectedCategory])
+
+  const handleImageClick = (url: string) => {
+    setSelectedImage(url)
+    setIsModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <S.Wrapper>
+      <MenuBar setSelectedCategory={setSelectedCategory} />
+      <List pictures={pictures} onImageClick={handleImageClick} />
+      {isModalOpen && (
+        <S.Modal onClick={handleCloseModal}>
+          <S.ModalContent onClick={(e) => e.stopPropagation()}>
+            <img src={selectedImage} alt="" />
+          </S.ModalContent>
+        </S.Modal>
+      )}
     </S.Wrapper>
   )
 }
