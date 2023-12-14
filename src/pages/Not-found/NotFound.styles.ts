51f43a86c340531b75bab1f65457ace7fb7886d6
yunshin <2younsin@gmail.com>
2023-12-15 00:29:23 +0900

    chore: 404Page 내부 리소스가 스크린 수직 중앙에 위치하도록 변경

--- src/pages/Not-found/NotFound.styles.ts
+++ src/pages/Not-found/NotFound.styles.ts
@@ -3,7 +3,7 @@ import { styled } from 'styled-components'
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 80vh;
   ${FlexCenterCSS}
   ${FlexColumnCSS}
   gap: 20px;
