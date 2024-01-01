264e3c2261bb0c5b1cb8e99623793ede022b0d4b
Team <50646145+TransparentDeveloper@users.noreply.github.com>
2024-01-01 19:09:34 +0900

    fix: techwiki api url 수정

--- src/services/apis/axios.ts
+++ src/services/apis/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const baseURL = `${import.meta.env.VITE_APP_SERVER_URL}/api`
+const baseURL = `${import.meta.env.VITE_APP_API_SERVER_URL}`
 
 export const axiosInstance = axios.create({ baseURL })
