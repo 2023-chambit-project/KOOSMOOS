34b287d0cd6a8975933d1255ea3bc0c942886b0e
yunshin <2younsin@gmail.com>
2024-01-07 02:39:21 +0900

    refactor: 불필요하게 명시된 'return' 삭제

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -4,14 +4,10 @@ import { useSetRecoilState } from 'recoil'
 export const useModal = () => {
   const setModalProps = useSetRecoilState(modalPropsAtom)
   const onClose = () => {
-    setModalProps((prev) => {
-      return { ...prev, isOpen: false }
-    })
+    setModalProps((prev) => ({ ...prev, isOpen: false }))
   }
   const onModalOpen = ({ ...props }) => {
-    setModalProps((prev) => {
-      return { ...prev, ...props, isOpen: true, onClose }
-    })
+    setModalProps((prev) => ({ ...prev, ...props, isOpen: true, onClose }))
   }
   return { open: onModalOpen }
 }
