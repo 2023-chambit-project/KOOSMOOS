04698497fbf2c1a0a60fb887682bc4db5471cdab
yunshin <2younsin@gmail.com>
2024-01-07 00:09:56 +0900

    fix: axios 호출 시마다 환경변수 적용토록 수정

--- src/services/apis/axios.ts
+++ src/services/apis/axios.ts
@@ -2,4 +2,4 @@ import axios from 'axios'
 
 const baseURL = `${import.meta.env.VITE_APP_API_SERVER_URL}`
 
-export const axiosInstance = axios.create({ baseURL })
+export const axiosInstance = () => axios.create({ baseURL })
--- src/services/apis/techWiki.ts
+++ src/services/apis/techWiki.ts
@@ -2,11 +2,11 @@ import { TechWikiPostProps, TechWikiSummaryListProps } from '@/types'
 import { axiosInstance } from './axios'
 
 export const getTechWikiSummaryList = async () => {
-  const res = await axiosInstance.get<TechWikiSummaryListProps>('/tech-wikis')
+  const res = await axiosInstance().get<TechWikiSummaryListProps>('/tech-wikis')
   return res.data
 }
 
 export const getTechWikiPost = async (id?: string) => {
-  const res = await axiosInstance.get<TechWikiPostProps>(`/tech-wikis/${id}`)
+  const res = await axiosInstance().get<TechWikiPostProps>(`/tech-wikis/${id}`)
   return res.data
 }
