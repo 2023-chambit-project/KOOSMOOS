014009f4ab4f0c61d338a508704627c8a0e3489d
yunshin <2younsin@gmail.com>
2024-01-07 03:03:19 +0900

    refactor: 모달 사용시, 필수 파라미터 자동완성 되도록 타입명시

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,13 +1,14 @@
 import { modalPropsAtom } from '@/atoms/modalProps.atom'
+import type { ModalProps } from '@/atoms/modalProps.type'
 import { useSetRecoilState } from 'recoil'
 
 export const useModal = () => {
-  const setModalProps = useSetRecoilState(modalPropsAtom)
+  const setModalProps = useSetRecoilState<ModalProps>(modalPropsAtom)
   const onClose = () => {
     setModalProps((prev) => ({ ...prev, isOpen: false }))
   }
-  const onModalOpen = ({ ...props }) => {
-    setModalProps((prev) => ({ ...prev, ...props, isOpen: true, onClose }))
+  const onModalOpen = ({ message }: Pick<ModalProps, 'message'>) => {
+    setModalProps((prev) => ({ ...prev, message, isOpen: true, onClose }))
   }
   return { open: onModalOpen }
 }
